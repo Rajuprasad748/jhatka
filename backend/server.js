79791d7be3548e34295819411d42dd37b15698c8
Raju Prasad <rp0071085@gmail.com>
2025-09-20 21:04:27 +0530

    comp

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = ["https://userfrontend-vpmj.onrender.com", "https://adminfrontend-zcij.onrender.com"];
 
 app.use(
   cors({
