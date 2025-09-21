d942cbb5aec300b0187abbf7a8c25e143386b017
Raju Prasad <rp0071085@gmail.com>
2025-09-21 11:58:11 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [`https://localhost:5173`, `https://localhost:5174`];
+const allowedOrigins = [`https://userfrontend-xnvv.onrender.com/`, `https://adminbackend-z7tn.onrender.com/`];
 
 app.use(
   cors({
