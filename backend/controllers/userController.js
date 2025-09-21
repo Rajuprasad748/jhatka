015795881a45f182312d5090aa493f70d7713dc1
Raju Prasad <rp0071085@gmail.com>
2025-09-21 14:29:48 +0530

    g

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,7 +78,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production" ? true : false,
-      sameSite: "none",
+      sameSite: "None",
       maxAge: 60 * 60 * 1000,
     });
 
