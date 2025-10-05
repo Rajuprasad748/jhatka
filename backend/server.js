b34c4fa0eb08df40b5e8ab60635774c308e181ae
Raju Prasad <rp0071085@gmail.com>
2025-10-06 02:37:22 +0530

    dd

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [`https://userfrontend-xnvv.onrender.com`, `https://royal10xadmin.onrender.com/`];
+const allowedOrigins = [`https://userfrontend-xnvv.onrender.com`, `https://royal10xadmin.onrender.com`];
 
 app.use(
   cors({
